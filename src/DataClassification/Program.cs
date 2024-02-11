ed5c57976362c971fc6ae6e8fb7fcf3d1ae8a960
gurkangur <gurkangur154@gmail.com>
2024-02-11 10:10:22 +0300

    init

---
+++ src/DataClassification/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataClassification
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
